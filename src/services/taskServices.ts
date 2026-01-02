054c437cc20ae75a045d016c0d2f164a0a98c368
Shaik Mahammad Rafi <rafi.shaik@everest.engineering>
2026-01-02 17:30:37 +0530

    Implement addTask service which add task to database

--- src/services/taskServices.ts
+++ src/services/taskServices.ts
@@ -1,3 +1,4 @@
+import type { task } from "../types/task.js";
 import { db } from "../db-config/firebaseConfig.js";
 
 let TasksCollection = db.collection("todo-collection");
@@ -7,3 +8,7 @@ export const getAllTasks = async()=>{
     return tasks.docs.map(doc => ({id : doc.id , ...doc.data()}))
 }
 
+export const addNewTask = async(Task : task)=>{
+    const newTask = await TasksCollection.add(Task)
+    return {id:newTask.id , ...Task}
+}
