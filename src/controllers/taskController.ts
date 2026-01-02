b80848cfdd88175e3cd9eb416a10d5e348d7182e
Shaik Mahammad Rafi <rafi.shaik@everest.engineering>
2026-01-02 17:31:45 +0530

    Implement addTask controller

--- src/controllers/taskController.ts
+++ src/controllers/taskController.ts
@@ -1,5 +1,6 @@
 import type { Request, Response } from "express";
 import { addNewTask, getAllTasks } from "../services/taskServices.js";
+import type { task } from "../types/task.js";
 
 export const getTasks = async (request: Request, response: Response) => {
     try {
@@ -8,5 +9,15 @@ export const getTasks = async (request: Request, response: Response) => {
     }
     catch {
         response.status(500).send({ error: "Failed to fetch task from the database" })
+    }
+}
+
+export const addTask = async (request: Request, response: Response) => {
+    try {
+        const Task: task = request.body;
+        const newTask = await addNewTask(Task);
+        response.status(201).json(newTask);
+    } catch (error) {
+        response.status(500).json({ error: "Failed to add task" });
     }
 }
