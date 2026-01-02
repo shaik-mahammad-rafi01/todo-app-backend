2e3cefce3fc78a4be28ef766fe63ccfab7be397c
Shaik Mahammad Rafi <rafi.shaik@everest.engineering>
2026-01-02 13:59:59 +0530

    Initial project set-up with basic README

---
+++ src/db-config/firebaseConfig.ts
@@ -0,0 +1,9 @@
+import  { initializeApp,cert } from 'firebase-admin/app';
+import { getFirestore } from 'firebase-admin/firestore';
+import fs from 'fs';
+const serviceAccount =JSON.parse(fs.readFileSync('db-config.json').toString());
+initializeApp({
+  credential: cert(serviceAccount)
+});
+export const db = getFirestore()
+
