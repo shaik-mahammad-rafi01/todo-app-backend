fdc4c42c3dbf90e0faeb0e8f9165a0198e49865f
Shaik Mahammad Rafi <rafi.shaik@everest.engineering>
2026-01-04 15:44:21 +0530

    Implement db mocking and addTask testase

---
+++ src/services/taskServices.test.ts
@@ -0,0 +1,45 @@
+import { addNewTask, deleteTaskFromDB, editTaskService, getAllTasks } from "./taskServices";
+jest.mock("../db-config/firebaseConfig", () => ({
+    collection: jest.fn(() => ({
+        get: jest.fn(() => ({
+            docs: [
+
+                { id: "123", data: jest.fn(() => ({ name: "data" })) }
+
+            ]
+        }))
+        ,
+        add: jest.fn(() => ({ id: "628142" })),
+        doc: jest.fn(() => ({
+            get: jest.fn(() => ({
+                exists: true,
+                data: jest.fn(() => ({
+                    taskName: "new todo app",
+                    priority: "low",
+                    status: "pending",
+                    description: "new app",
+                    Deadline: "03-01-2026"
+                }))
+            })),
+            update: jest.fn(),
+            delete: jest.fn()
+
+        }))
+    }))
+
+
+}))
+describe("Task services test cases", () => {
+    test("addTask", async () => {
+
+        const task = await addNewTask({
+            priority: 'high',
+            description: 'native',
+            status: 'pending',
+            taskName: 'todo',
+            Deadline: '04-1-2026'
+        });
+        expect(task.id).toBe("628142");
+        expect(task.taskName).toBe("todo")
+    })
+})
