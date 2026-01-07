f5159bba007efa24fb75edcdae870b84897bbb14
Shaik Mahammad Rafi <rafi.shaik@everest.engineering>
2026-01-08 00:08:48 +0530

    Add test case for controller to editTask with task id

--- src/controllers/taskController.test.ts
+++ src/controllers/taskController.test.ts
@@ -17,9 +17,15 @@ jest.mock("../services/taskServices", () => ({
         Deadline: "03-01-2026"
     })),
 
-    deleteTaskFromDB: jest.fn(()=>"Task deleted")
-
-
+    deleteTaskFromDB: jest.fn(() => "Task deleted"),
+    editTaskService: jest.fn(() => ({
+        id: "123",
+        taskName: "new todo app",
+        priority: "low",
+        status: "pending",
+        description: "new app",
+        Deadline: "03-01-2026"
+    }))
 }))
 describe("controllers test cases", () => {
 
@@ -48,9 +54,9 @@ describe("controllers test cases", () => {
         expect(result.body).toEqual(task)
     })
 
-    test("It should delete task if exist", async() => {
+    test("It should delete task if exist", async () => {
         const task = {
-            id:"123",
+            id: "123",
             taskName: "new todo app",
             priority: "low",
             status: "pending",
@@ -59,5 +65,19 @@ describe("controllers test cases", () => {
         };
         const result = await request(app).delete("/deleteTask/123").send(task)
         expect(result.text).toBe("Task deleted")
+    })
+
+    test("It should edit an existing task updated data", async () => {
+        const task = {
+            id: "123",
+            taskName: "new todo app",
+            priority: "low",
+            status: "pending",
+            description: "new app",
+            Deadline: "03-01-2026"
+        };
+
+        const result = await request(app).put("/editTask/123").send(task)
+        expect(result.body).toEqual(task)
     })
 })
